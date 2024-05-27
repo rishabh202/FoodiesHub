5d5a2ebf8baec1479db2d13d3e4188745ce8f7f2
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-05-27 18:12:35 +0530

    done some work with header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+ import styled from "styled-components"
+
 function App() {
 
   return (
-    <>
-     
-    </>
+    <MainContainer>
+      
+    <TopContainer>
+        <div className="logo"><img src="/logo.png" alt="FoodiesHub logo" /></div>
+        <div className="search"><input type="text" placeholder="Search Food" /></div>
+    </TopContainer>
+    </MainContainer>
   )
 }
 
-export default App
+export default App;
+
+const MainContainer = styled.div`
+background-color: rgba(50, 51, 52, 1);
+`;
+const TopContainer = styled.div`
+  
+`;
