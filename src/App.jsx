2f57372dbdff8ed3c81bacb9e0de535817d75c5b
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-06-03 20:04:51 +0530

    updated the backend link to project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 import SearchResults from "./components/SearchResults";
 
-export const BASE_URL = "http://localhost:9000";
+export const BASE_URL = "https://foodieshubbackend-1.onrender.com/";
 
 function App() {
   const [data, setData] = useState(null);
